84384c14a4a76729068c2f46a2b5db604eb89851
hericksilva9 <hericksilva9@outlook.com>
2025-09-04 18:06:43 -0300

    Adiciona Chaotic-AUR

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -7,6 +7,21 @@ trap 'kill $SUDO_PID; sudo -k' EXIT
 # Update system packages
 sudo pacman -Syu
 
+# Import Chaotic-AUR GPG key
+sudo pacman-key --recv-keys 3056513887B78AEB
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+# Install Chaotic-AUR keyring and mirrorlist
+sudo pacman -U --noconfirm \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+  'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# Add Chaotic-AUR repository to pacman.conf if it doesn't exist yet
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+  echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | \
+    sudo tee -a /etc/pacman.conf
+fi
+
 # Install audio servers and dependencies
 sudo pacman -S pipewire-alsa pipewire-jack pipewire-pulse wireplumber gstreamer --noconfirm
 
@@ -21,16 +36,6 @@ sudo pacman -S dolphin dolphin-plugins ark kio-admin polkit-kde-agent --noconfir
 
 # Install keyring manager
 sudo pacman -S gnome-keyring --noconfirm
-
-# Install yay
-pushd "$HOME"
-sudo pacman -S --needed base-devel git go
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd "$HOME"
-rm -rf yay/
-popd
 
 # Install hyprland and related components
 sudo pacman -S hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker waybar rofi-wayland --noconfirm
@@ -51,6 +56,17 @@ sudo pacman -S ttf-font-awesome ttf-jetbrains-mono-nerd ttf-opensans noto-fonts 
 # Install additional utilities
 sudo pacman -S dunst cliphist mpv pavucontrol --noconfirm
 
+# # Install yay
+# pushd "$HOME"
+# sudo pacman -S --needed base-devel git go
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
+# cd "$HOME"
+# rm -rf yay/
+# popd
+
 # Install Apps
-yay -S google-chrome --noconfirm  # browser
+sudo pacman -S yay --noconfirm    # yay
 sudo pacman -S code --noconfirm   # VSCode
+yay -S google-chrome --noconfirm  # browser
