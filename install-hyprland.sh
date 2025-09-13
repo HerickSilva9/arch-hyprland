c8f1cdf249815dc2525163b9894f001a007b56db
HerickSilva9 <hericksilva9@outlook.com>
2025-09-12 21:31:53 -0300

    Remove configurações antigas de instalação de pacotes e escolha de profile em archinstall

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -24,47 +24,10 @@ fi
 
 sudo pacman -Syu --noconfirm
 
-# Install audio servers and dependencies
-sudo pacman -S --noconfirm pipewire-alsa pipewire-jack pipewire-pulse wireplumber gstreamer
-
-# Install multimedia codecs
-sudo pacman -S --noconfirm gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg
-
-# Install terminal emulator
-sudo pacman -S --noconfirm kitty
-
-# Install file manager and related tools
-sudo pacman -S --noconfirm dolphin dolphin-plugins ark kio-admin polkit-kde-agent
-
-# Install keyring manager
-sudo pacman -S  --noconfirm gnome-keyring
-
-# Install hyprland and related components
-sudo pacman -S --noconfirm hyprland hyprlock hypridle hyprcursor hyprpaper hyprpicker waybar rofi-wayland
-
-# Copy hyprland config file
-mkdir -p "$HOME/.config/hypr"
-cp hyprland/hyprland.conf "$HOME/.config/hypr"
-
-# Install Qt wayland support
-sudo pacman -S --noconfirm qt5-wayland qt6-wayland
-
-# Install XDG portals and user directories integration
-sudo pacman -S --noconfirm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs-gtk
-
-# Install fonts
-sudo pacman -S --noconfirmttf-font-awesome ttf-jetbrains-mono-nerd ttf-opensans noto-fonts ttf-droid ttf-roboto
-
-# Install additional utilities
-sudo pacman -S --noconfirm dunst cliphist mpv pavucontrol
+sudo pacman -S --noconfirm gnome-keyring libsecret
 
 # Install Apps
-sudo pacman -S --noconfirm yay                      # yay
 sudo pacman -S --noconfirm visual-studio-code-bin   # VSCode
 sudo pacman -S --noconfirm google-chrome            # browser
 sudo pacman -S --noconfirm neofetch
 sudo pacman -S --noconfirm tree
-
-# Themes and icons
-sudo pacman -S --noconfirm breeze breeze5 breeze-gtk papirus-icon-theme
-sudo pacman -S --noconfirm nwg-look
