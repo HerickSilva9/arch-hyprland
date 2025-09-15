e182dbee88a0f4ba589bb6a2481ef2cab56ae75d
HerickSilva9 <hericksilva9@outlook.com>
2025-09-15 15:13:55 -0300

    wifi

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -48,6 +48,8 @@ sudo systemctl enable docker.socket
 
 # Install virt-manager
 sudo pacman -S --noconfirm virt-manager qemu-desktop libvirt edk2-ovmf vde2 bridge-utils openbsd-netcat dnsmasq dmidecode
-sudo systemctl enable libvirtd.service
+sudo systemctl enable --now libvirtd.service
+
+sudo systemctl enable --now wpa_supplicant@wlan0.service
 
 echo "Restart your pc to apply the changes."
--- wifi/README.md
+++ wifi/README.md
@@ -2,5 +2,5 @@ Copy the `wpa_supplicant.conf` file to path `/etc/wpa_supplicant/` and change th
 
 Execute the command bellow to connect
 ```
-wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 ```
